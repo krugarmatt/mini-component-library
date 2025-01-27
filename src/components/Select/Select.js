688ca81c5ad7b5f34c70b5c9625bd54260d51a78
Matthew Ross <matthewross91@gmail.com>
2025-01-27 08:52:26 +0000

    Implement Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,14 +5,50 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
+const Select = ({ id, label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect id={id} value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      {displayedValue}
+      <Icon id={"chevron-down"} size={24} strokeWidth={2} />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  display: inline-flex;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 18.75px;
+  gap: 18px;
+  position: relative;
+  padding: 8px 12px 8px 16px;
+
+  :hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const NativeSelect = styled.select`
+  border-radius: inherit;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  appearance: none;
+  border: 0;
+  padding: 0;
+  background-color: transparent;
+  text-indent: -9999px;
+`;
 
 export default Select;
